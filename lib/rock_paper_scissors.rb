6db325cf24affa5633fff6cbcb4ff64a34013036
Ryan Stantz <ryanstantz@gmail.com>
2016-03-24 15:48:14 -0500

    Tested and working just fine, sans testing on 'puts' methods. Also needing method documentation. Done with Phase 1.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,19 +1,38 @@
 require 'pry'
-#Tells us if our rock-paper-scissors move is valid.
-#
-#Takes in a string as an argeument and compares it to the strings "rock", "paper", and "scissors"
-#
-#Returns true if valid or false if not valid.
-  def valid_move?(move)
-    valid = ['r', 'rock', 'p', 'paper', 's', 'scissors']
-    valid.include? move.downcase
-  end
 
-
-#Tells us who our Rock,Paper,Scissor winner is.
-#
-#Takes in a valid argu
-#
-#
-def return_winner(move1, move2)
+def valid_move?(move)
+  valid = ['rock', 'paper', 'scissors']
+  valid.include? move.downcase
 end
+
+def get_move(player)
+  puts "\n#{player}, what is your weapon of choice?\n"
+  move = gets.chomp
+  while !valid_move?(move)
+    puts "\n#{player}, what is your VALID weapon of choice?\n"
+    move = gets.chomp
+  end
+  move
+end
+
+def return_winner(player_1, player_2)
+    winner = ''
+    if player_1 == 'rock'     && player_2 == 'scissors' ||
+       player_1 == 'scissors' && player_2 == 'paper' ||
+       player_1 == 'paper'    && player_2 == 'rock'
+       winner = 'player_1'
+
+     elsif player_2 == 'rock' && player_1 == 'scissors' ||
+       player_2 == 'scissors' && player_1 == 'paper' ||
+       player_2 == 'paper' && player_1 == 'rock'
+       winner = 'player_2'
+
+     else
+      winner = 'tie'
+     end
+end
+
+def rock_paper_scissors(p1_name, p1_move, p2_name, p2_move)
+  champ = return_winner(p1_move,p2_move)
+end
+binding.pry
--- tests/rock_paper_scissors_test.rb
+++ tests/rock_paper_scissors_test.rb
@@ -3,20 +3,20 @@ require_relative '../lib/rock_paper_scissors.rb'
 
 class RockPaperScissorsTest < Minitest::Test
 
-  def test_valid_answer_with_r
-    move = 'r'
+  def test_valid_answer_with_rock
+    move = 'rock'
     actual = valid_move?(move)
     assert_equal(true, actual, "'true' as a boolean should be returned.")
   end
 
-  def test_valid_answer_with_capitol_R
-    move = 'R'
+  def test_valid_answer_with_capitol_rock
+    move = 'Rock'
     actual = valid_move?(move)
     assert_equal(true, actual, "'true' as a boolean should be returned.")
   end
 
   def test_valid_answer_with_s
-    move = 's'
+    move = 'scissoRS'
     actual = valid_move?(move)
     assert_equal(true, actual, "'true' as a boolean should be returned.")
   end
@@ -28,14 +28,34 @@ class RockPaperScissorsTest < Minitest::Test
   end
 
   def test_valid_answer_with_p
-    move = 'R'
+    move = 'paper'
     actual = valid_move?(move)
     assert_equal(true, actual, "'true' as a boolean should be returned.")
   end
 
-  def test_valid_answer_with_rock
+  def test_invalid_answer
     move = 'Rake'
     actual = valid_move?(move)
     refute_equal(true, actual, "'false' as a boolean should be returned.")
+  end
+
+  def test_return_winner_rock_rock
+    actual = return_winner('rock', 'rock')
+    assert_equal('tie', actual, "'tie' as a string should be returned.")
+  end
+
+  def test_return_winner_rock_scissors
+    actual = return_winner('rock', 'scissors')
+    assert_equal('player_1', actual, "'player_1' as a string should be returned.")
+  end
+
+  def test_return_winner_rock_scissors
+    actual = return_winner('paper', 'scissors')
+    assert_equal('player_2', actual, "'player_2' as a string should be returned.")
+  end
+
+  def test_get_name
+    actual = get_name('Ryan')
+    assert_equal('Ryan', actual, "'Ryan' as a string should be returned.")
   end
 end
